b14465026ef165a9197ffcd183aaf6bfd4775e32
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-05-08 22:54:50 +0300

    fix: separate logo to 2 pictures

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './../../assets/img/logo.svg'
+import part1 from './../../assets/img/part1.svg'
+import part2 from './../../assets/img/part2.svg'
 
 const Logo = () => {
   return (
     <div className='logo last'>
-      <img src={logo} alt="logo" className="logo__img" />
+    <div className="logo__img">
+      <img src={part1} alt="logo" className="logo__img_part1" />
+      <img src={part2} alt="logo" className="logo__img_part2" />
+    </div>
       <p className="logo__text">
         крупный интегратор CRM в Росcии и ещё 8 странах
       </p>
