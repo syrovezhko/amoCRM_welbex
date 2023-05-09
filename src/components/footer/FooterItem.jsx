8ffc9d3f13049a899e97f18423ee0c5f58941e4b
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-05-10 00:05:28 +0300

    fix: footer `adaptive`

--- src/components/footer/FooterItem.jsx
+++ src/components/footer/FooterItem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Media from 'react-media'
 
-const FooterItem = (text) => {
-  console.log('text', text)
+const FooterItem = ({text, id}) => {
+  let displayText = id === 7 ? "Благодарность клиентов" : text
   return (
-    <p className='footer-item'>
-      {text.text}
-    </p>
+    <Media queries={{ small: { maxWidth: 1050 } }}>
+    {matches => 
+      matches.small ? (
+        <p className='footer-item'>
+          {displayText}
+        </p>
+      )
+      : (
+        <p className='footer-item'>
+          {text}
+        </p>
+      )
+    }
+  </Media>
   );
 };
 
--- src/components/footer/FooterList.jsx
+++ src/components/footer/FooterList.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import FooterItem from './FooterItem';
 
 const FooterList = ({title, list}) => {
-  console.log(list)
   return (
     <div className='footer-list'>
       <h3 className="footer-list__title">{title}</h3>
       <div className="footer-list__block">
-        {list.map((text) =>
-          <FooterItem text={text.text}/>
+        {list.map((i) =>
+          <FooterItem
+            text={i.text}
+            key={i.id}
+            id={i.id}/>
         )}
       </div>
     </div>
